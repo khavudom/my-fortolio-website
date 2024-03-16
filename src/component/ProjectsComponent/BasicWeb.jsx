4ca26ed42b6298bf38217326a7f82f17cc1f1339
RuenChanPiseth <127710222+RuenChanPiseth@users.noreply.github.com>
2024-03-17 00:16:20 +0700

    add loading animating

--- src/component/ProjectsComponent/BasicWeb.jsx
+++ src/component/ProjectsComponent/BasicWeb.jsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
+import Loading from "./Loading";
 const api = "https://expres-server-test-v1.vercel.app";
 const BasicWeb = () => {
   const [data, setData] = useState([]);
-  const [couter, setCounter] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetch(api)
       .then((res) => res.json())
-      .then((data) => setData(data))
-      .catch((error) => console.error(error));
+      .then((data) => {
+        setIsLoading(false);
+        setData(data);
+      })
+      .catch((error) => {
+        setIsLoading(false);
+      });
   }, []);
 
   return (
@@ -25,55 +31,59 @@ const BasicWeb = () => {
       </div>
 
       {/* 1th */}
-      <div className="flex flex-col">
-        {data.map(
-          ({
-            projectName,
-            index,
-            gitHubSrc,
-            productSrc,
-            explaination,
-            _id,
-          }) => {
-            return (
-              <div className="w-full py-4" key={_id}>
-                <article className=" project-view">
-                  <div className="sm:float-left sm:pr-4">
-                    <h2>
-                      {index}. {projectName}
-                    </h2>
-                    <div className="my-4  max-w-[550px] max-h-[550px] ] ">
-                      <iframe
-                        className="w-full h-full bg-white min-h-[200px]"
-                        src={productSrc}
-                        frameBorder="0"></iframe>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="flex flex-col">
+          {data.map(
+            ({
+              projectName,
+              index,
+              gitHubSrc,
+              productSrc,
+              explaination,
+              _id,
+            }) => {
+              return (
+                <div className="w-full py-4" key={_id}>
+                  <article className=" project-view">
+                    <div className="sm:float-left sm:pr-4">
+                      <h2>
+                        {index}. {projectName}
+                      </h2>
+                      <div className="my-4  max-w-[550px] max-h-[550px] ] ">
+                        <iframe
+                          className="w-full h-full bg-white min-h-[200px]"
+                          src={productSrc}
+                          frameBorder="0"></iframe>
+                      </div>
+                      <aside className="flex gap-3">
+                        <a
+                          className="text-blue-400 underline"
+                          href={gitHubSrc}
+                          target="_blank">
+                          Download source code
+                        </a>
+                        <a
+                          href={productSrc}
+                          target="_blank"
+                          className="text-blue-500 underline">
+                          View Full Project
+                        </a>
+                      </aside>
                     </div>
-                    <aside className="flex gap-3">
-                      <a
-                        className="text-blue-400 underline"
-                        href={gitHubSrc}
-                        target="_blank">
-                        Download source code
-                      </a>
-                      <a
-                        href={productSrc}
-                        target="_blank"
-                        className="text-blue-500 underline">
-                        View Full Project
-                      </a>
-                    </aside>
-                  </div>
 
-                  <div className="text-justify pt-3 sm:pt-12">
-                    <h3 className="py-2">Explaination</h3>
-                    {explaination}
-                  </div>
-                </article>
-              </div>
-            );
-          }
-        )}
-      </div>
+                    <div className="text-justify pt-3 sm:pt-12">
+                      <h3 className="py-2">Explaination</h3>
+                      {explaination}
+                    </div>
+                  </article>
+                </div>
+              );
+            }
+          )}
+        </div>
+      )}
       {/* 2th */}
     </div>
   );
---
+++ src/component/ProjectsComponent/Loading.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="w-[50px] h-[50px] rounded-[50%] border-[6px] border-gray-700 border-t-blue-500 border-solid mx-auto my-6 animate-spin "></div>
+  );
+};
+
+export default Loading;
